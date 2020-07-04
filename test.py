5641c0bf442e4c3aa898676d381e8b2ee749d6e0
NKH123 <neerajd@google.com>
2020-07-05 04:24:44 +0530

    Add something

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from google.cloud import storage
 
 bucket_name = "automation-interns"
-destination_file_name = ("./text.txt")
-source_blob_name = "test/text_file.txt"
+destination_file_name = ("./test/")
+source_blob_name = "test/"
 storage_client = storage.Client()
 bucket = storage_client.bucket(bucket_name)
 blob = bucket.blob(source_blob_name)
