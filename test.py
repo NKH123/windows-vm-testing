749485c599eff13275396c961fa4e3e16ce6f35e
NKH123 <neerajd@google.com>
2020-07-05 20:14:20 +0530

    Something

--- test.py
+++ test.py
@@ -6,4 +6,5 @@ source_blob_name = "test/"
 storage_client = storage.Client()
 bucket = storage_client.bucket(bucket_name)
 blob = bucket.blob(source_blob_name)
-blob.download_to_filename(destination_file_name)
+# blob.download_to_filename(destination_file_name)
+bucket.list_blobs()
