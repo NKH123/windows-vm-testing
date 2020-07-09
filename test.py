1df22ff616d6318ebfdf9d952c45e08df74f55ab
Neeraj Deshpande <neerajd@google.com>
2020-07-09 01:41:27 +0000

    Made change

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from google.cloud import storage
 
 bucket_name = "automation-interns"
-destination_file_name = ("./test")
-source_blob_name = "test/"
+destination_file_name = ("./stuff.txt")
+source_blob_name = "stuff.txt"
 storage_client = storage.Client()
 bucket = storage_client.bucket(bucket_name)
 blob = bucket.blob(source_blob_name)
