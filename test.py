4be4b0dd9673bdf2b44f4c7a91ea8458f71fab3f
NKH123 <neerajd@google.com>
2020-07-09 07:35:09 +0530

    list blobs

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ bucket_name = "automation-interns"
 destination_file_name = ("./stuff.txt")
 source_blob_name = "stuff.txt"
 storage_client = storage.Client()
-bucket = storage_client.bucket(bucket_name)
-blob = bucket.blob(source_blob_name)
-# blob.download_to_filename(destination_file_name)
-bucket.list_blobs()
+blobs = storage_client.list_blobs(bucket_name)
+for blob in blobs:
+  print(blob.name)
